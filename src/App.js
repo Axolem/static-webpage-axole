7ad9b2451b77b53d62b0f63ab7c20080af5b1cfa
Axole Maranjana <axolemaranjana4@gmail.com>
2022-06-27 01:46:55 +0200

    Edited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Axoe.
-        </p>
-        <a
-          className="App-link"
-          href="https://"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originallt created by Jordan Walker</li>
+        <li>Has over 100K stars in Github</li>
+        <li>It is maintained by Facebook</li>
+        <li>Use in thousands of enterprice apps including mobile apps</li>
+      </ul>
     </div>
   );
 }
