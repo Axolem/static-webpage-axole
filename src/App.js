3de423c37d226cb4c9860ee7736fd2cbb3e7c263
Axole Maranjana <axolemaranjana4@gmail.com>
2022-07-02 10:04:14 +0200

    Updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+function Header() {
+  return (
+    <header>
+      <nav className="navbar">
+        <img className="App-logo" src={logo}  />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+function Footer() {
+  return (
+    <footer>
+      <hr></hr>
+      <p>Copyright © 2022 | Axole Maranjana</p>
+    </footer>
+  );
+}
+function MainContent() {
+  return (
+    <div>
+      <h1>Reasons I'm excited to learn react...</h1>
+      <ol>
+        <li>Simple</li>
+        <li>Declarative language</li>
+        <li>Large community</li>
+      </ol>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originallt created by Jordan Walker</li>
-        <li>Has over 100K stars in Github</li>
-        <li>It is maintained by Facebook</li>
-        <li>Use in thousands of enterprice apps including mobile apps</li>
-      </ul>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
